ee3e6ff9fff28f989c3988971335493ab6a3c51f
Sajan Gutta <gutta@usc.edu>
2020-12-17 14:54:44 -0500

    starting to add email support to application

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_caching import Cache
 from peewee import MySQLDatabase
 from flask_login import LoginManager
+from flask_mail import Mail
 import os
 
 
@@ -9,13 +10,25 @@ db = MySQLDatabase(os.environ.get("DB_NAME"), user=os.environ.get("DB_USER"), pa
                    host=os.environ.get("DB_HOST"))
 login_manager = LoginManager()
 cache = Cache(config={'CACHE_TYPE': 'simple'})
+mail = Mail()
+
+
+class Config(object):
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = [os.environ.get('MAIL_USERNAME')]
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'this-is-a-placeholder'
+    app.config.from_object(Config)
 
     login_manager.init_app(app)
+    mail.init_app(app)
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
