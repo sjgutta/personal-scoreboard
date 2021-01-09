c5c9a9db40859711ab82b6e9bf0ff8f35c2d85d1
Sajan Gutta <gutta@usc.edu>
2021-01-09 17:22:50 -0500

    adding log out url to admin

--- app/admin/app.py
+++ app/admin/app.py
@@ -1,4 +1,5 @@
 from flask_admin import Admin
+from flask_admin.menu import MenuLink
 from app.admin.users import UserAdmin
 from app.admin.teams import TeamAdmin
 
@@ -10,5 +11,7 @@ def create_admin():
     scoreboard_admin = Admin()
     scoreboard_admin.add_view(UserAdmin(User, name="Users", endpoint="users"))
     scoreboard_admin.add_view(TeamAdmin(Team, name="Teams", endpoint="teams"))
+
+    scoreboard_admin.add_link(MenuLink(name='Log Out', url="/logout", class_name='logout'))
 
     return scoreboard_admin
