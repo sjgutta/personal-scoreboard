afeec002f181f6e9d7ea18195acfaafb3118b24f
Sajan Gutta <gutta@usc.edu>
2021-01-10 18:06:53 -0500

    downloading logo images for soccer teams

--- scripts/download_logo_images.py
+++ scripts/download_logo_images.py
@@ -25,5 +25,16 @@ def download_ncaam_images():
         logo_url = team.logo_url
         urllib.request.urlretrieve(logo_url, f"ncaam_logo_images/{team.full_name}.png")
 
+def download_soccer_images():
+    from app.models.team import Team
+
+    print("running")
+    teams = Team.select().where(Team.sport_type == Sport.SportType.SOCCER)
+    print("found teams")
+    for team in teams:
+        print(team.full_name)
+        logo_url = team.logo_url
+        urllib.request.urlretrieve(logo_url, f"soccer_logo_images/{team.full_name}.png")
+
 if __name__ == "__main__":
     download_images()
