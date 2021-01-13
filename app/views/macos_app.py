5dc6430eac8434898c67e2ee8eff83465a37cbf0
Sajan Gutta <gutta@usc.edu>
2021-01-13 15:17:29 -0500

    fixing bug in providing download link

--- app/views/macos_app.py
+++ app/views/macos_app.py
@@ -6,4 +6,5 @@ from app.views import bp
 @bp.route('/macos-app', methods=['GET', 'POST'])
 def macos_app():
     download_link = "https://github.com/sjgutta/personal-scoreboard/releases/latest/download/personal-scoreboard.zip"
-    return render_template('views/macos_app.html', has_access=current_user.has_access, download_link=download_link)
+    has_access = current_user.is_authenticated and current_user.has_access
+    return render_template('views/macos_app.html', has_access=has_access, download_link=download_link)
