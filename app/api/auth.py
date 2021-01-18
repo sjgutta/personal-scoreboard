73aab48775e961a074a95db763655e6ccd519faa
Sajan Gutta <gutta@usc.edu>
2021-01-18 10:29:57 -0500

    updating version of latest release

--- app/api/auth.py
+++ app/api/auth.py
@@ -11,6 +11,6 @@ def restful_login_user():
     password = data.get("password")
     user = User.get_or_none(username=username)
     if user and user.check_password(password) and user.has_access:
-        return {"success": os.environ.get('SECRET_KEY'), "version": "1.3"}
+        return {"success": os.environ.get('SECRET_KEY'), "version": "1.4"}
     else:
         return {"error": "invalid credentials"}
